b138160521b956d58da8b5e952ddb1a30f756fd7
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-03-04 09:37:27 -0700

    feat: Make `echo` an HTTP request handler

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,45 @@
 import "jest-extended";
 import { echo } from "./index";
+import express from "express";
+import request from "supertest";
 
 describe("IP Echo", () => {
-	test("does nothing right now", () => {
-		expect(echo()).toBe("Hello, World!");
+	const app = express() //
+		.use("/", echo);
+
+	// TODO: Has a default export (because Vercel says so)
+	// TODO: Has Vercel Edge config
+
+	test("responds with 'Hello, World!'", async () => {
+		const response = await request(app)
+			.get("/")
+			.expect("Vary", "*")
+			.expect("Cache-Control", "no-store")
+			.expect("Content-Type", /json/u)
+			.expect(200);
+		expect(response.body).toBe("Hello, World!");
+	});
+
+	test("responds correctly to CORS preflight", async () => {
+		const response = await request(app)
+			.options("/")
+			.expect("Vary", "*")
+			.expect("Cache-Control", "no-store")
+			.expect("Access-Control-Allow-Origin", "*")
+			.expect(200);
+		expect(response.body).toStrictEqual({});
+	});
+
+	// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+	const BadMethods = ["HEAD", "POST", "PUT", "DELETE", "TRACE", "PATCH"] as const;
+	test.each(BadMethods)("responds 405 to %s requests", async rawMethod => {
+		const method = rawMethod.toLowerCase() as Lowercase<typeof rawMethod>;
+
+		const response = await request(app)
+			[method]("/")
+			.expect("Vary", "*")
+			.expect("Cache-Control", "no-store")
+			.expect(405);
+		expect(response.body).toStrictEqual({});
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
 // TODO: Make this into an Edge function that returns the caller's IP address
-export function echo(): string {
-	return "Hello, World!";
+export function echo(req: APIRequest, res: APIResponse): void {
+	setCommonHeaders(res);
+
+	switch (req.method?.toUpperCase()) {
+		// Normal requests:
+		case "GET":
+			res //
+				.status(200)
+				.json("Hello, World!")
+				.end();
+			break;
+
+		// CORS preflight:
+		case "OPTIONS":
+			res.status(200).end();
+			break;
+
+		// Everything else:
+		default:
+			res.status(405).end();
+			break;
+	}
+}
+
+function setCommonHeaders(res: APIResponse): void {
+	res
+		.setHeader("Cache-Control", "no-store")
+		.setHeader("Vary", "*") // See https://stackoverflow.com/a/54337073 for why "Vary: *" is necessary for Safari
+		.setHeader("X-Clacks-Overhead", "GNU Terry Pratchett")
+		.setHeader(
+			"Access-Control-Allow-Headers",
+			"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+		)
+		.setHeader("Access-Control-Allow-Origin", "*");
 }
