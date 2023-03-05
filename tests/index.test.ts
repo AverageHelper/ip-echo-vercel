e86c75048e4dc30c67ad5f2a2965c0df83379acd
James Robinson <1500092+AverageHelper@users.noreply.github.com>
2023-03-05 08:16:50 -0700

    chore: Break up the `echo` monolith (#7)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,18 +1,12 @@
 import "jest-extended";
-import { echo } from "../api/index";
+import endpoint from "../api/index";
 import fetchMock from "jest-fetch-mock";
-fetchMock.enableMocks();
+fetchMock.enableMocks(); // Enables use of `Request` and `Response` objects
 
 describe("IP Echo", () => {
 	const url = new URL("https://localhost/");
 	const IP_HEADER_NAME = "x-real-ip";
 	const TEST_IP = "::ffff:127.0.0.1";
-
-	test("has a default export that matches the named export", async () => {
-		// Vercel requires a default export
-		const unit = await import("../api/index");
-		expect(unit).toHaveProperty("default", echo);
-	});
 
 	test("has a Vercel Edge config", async () => {
 		const unit = await import("../api/index");
@@ -24,12 +18,14 @@ describe("IP Echo", () => {
 
 	test("responds correctly to CORS preflight", () => {
 		const req = new window.Request(url, { method: "OPTIONS" });
-		const response = echo(req);
+		const response = endpoint(req);
 		expect(response).toHaveProperty("headers");
 		expect(response.headers.get("Vary")).toBe("*");
 		expect(response.headers.get("Cache-Control")).toBe("no-store");
 		expect(response.headers.get("Access-Control-Allow-Origin")).toBe("*");
-		expect(response.status).toBe(200);
+		expect(response.headers.get("Access-Control-Allow-Methods")).toInclude("GET");
+		expect(response.headers.get("Access-Control-Allow-Headers")).toInclude("Accept");
+		expect(response.status).toBe(204);
 		expect(response.body).toBe(null);
 	});
 
@@ -37,7 +33,7 @@ describe("IP Echo", () => {
 	const BadMethods = ["HEAD", "POST", "PUT", "DELETE", "TRACE", "PATCH"] as const;
 	test.each(BadMethods)("responds 405 to %s requests", method => {
 		const req = new window.Request(url, { method });
-		const response = echo(req);
+		const response = endpoint(req);
 		expect(response).toHaveProperty("headers");
 		expect(response.headers.get("Vary")).toBe("*");
 		expect(response.headers.get("Cache-Control")).toBe("no-store");
@@ -46,8 +42,8 @@ describe("IP Echo", () => {
 	});
 
 	test("responds with the caller's IP address", async () => {
-		const req = new window.Request(url, { method: "GET", headers: { [IP_HEADER_NAME]: TEST_IP } });
-		const response = echo(req);
+		const req = new window.Request(url, { headers: { [IP_HEADER_NAME]: TEST_IP } });
+		const response = endpoint(req);
 		expect(response).toHaveProperty("headers");
 		expect(response.headers.get("Vary")).toBe("*");
 		expect(response.headers.get("Cache-Control")).toBe("no-store");
@@ -58,10 +54,12 @@ describe("IP Echo", () => {
 
 	test("responds with the caller's IP address in JSON", async () => {
 		const req = new window.Request(url, {
-			method: "GET",
-			headers: { [IP_HEADER_NAME]: TEST_IP, Accept: "application/json" },
+			headers: {
+				[IP_HEADER_NAME]: TEST_IP,
+				Accept: "application/json",
+			},
 		});
-		const response = echo(req);
+		const response = endpoint(req);
 		expect(response).toHaveProperty("headers");
 		expect(response.headers.get("Vary")).toBe("*");
 		expect(response.headers.get("Cache-Control")).toBe("no-store");
@@ -71,8 +69,8 @@ describe("IP Echo", () => {
 	});
 
 	test("responds with 404 if there is no 'X-Real-IP' header", () => {
-		const req = new window.Request(url, { method: "GET" });
-		const response = echo(req);
+		const req = new window.Request(url);
+		const response = endpoint(req);
 		expect(response.status).toBe(404);
 		expect(response.body).toBe(null);
 	});
