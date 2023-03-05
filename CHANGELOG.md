9f7441d48933dc5708ea4e027135cbe9d161d97c
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2023-03-05 08:14:17 -0700

    chore: Break up the `echo` monolith

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.1] - 2023-03-05
+
+### Added
+
+- Responses now include the `Access-Control-Allow-Methods` header, with a value of `GET, OPTIONS`.
+
+### Removed
+
+- Responses no longer include `X-CSRF-Token`,`X-Requested-With`, `Accept-Version`, `Content-MD5`, or `X-Api-Version` in the `Access-Control-Allow-Headers` header.
+- Responses no longer include CORS headers unless the request method is `OPTIONS` (ostensibly a CORS preflight request).
+
 ## [2.0.0] - 2023-03-05
 
 ### Changed
@@ -34,6 +45,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Vercel Edge function to echo the caller's IP address.
 
+[2.0.1]: https://github.com/AverageHelper/ip-echo-vercel/compare/v2.0.0...v2.0.1
 [2.0.0]: https://github.com/AverageHelper/ip-echo-vercel/compare/v1.0.2...v2.0.0
 [1.0.2]: https://github.com/AverageHelper/ip-echo-vercel/compare/v1.0.1...v1.0.2
 [1.0.1]: https://github.com/AverageHelper/ip-echo-vercel/compare/v1.0.0...v1.0.1
--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,51 @@
 import { ipAddress } from "@vercel/edge";
 
-export function echo(req: Request): Response {
-	switch (req.method?.toUpperCase()) {
-		// Normal requests:
-		case "GET": {
-			const ip = ipAddress(req);
-			if (!ip) {
-				return new Response(undefined, {
-					status: 404,
-					headers: { ...headers, "Content-Type": "application/json" },
-				});
-			}
+function echo(req: Request): Response {
+	const ip = ipAddress(req);
+	if (!ip) {
+		// No IP address found in headers (or wherever Vercel Edge likes to put those)
+		return new Response(undefined, { status: 404, headers });
+	}
 
-			const accept = req.headers.get("accept");
-			if (accept?.includes("application/json")) {
-				return new Response(JSON.stringify(ip).concat("\n"), {
-					status: 200,
-					headers: { ...headers, "Content-Type": "application/json" },
-				});
-			}
-			return new Response(ip.concat("\n"), {
-				status: 200,
-				headers: { ...headers, "Content-Type": "text/html" },
-			});
-		}
+	// Found IP, respond the way the caller wants us to:
+	const accept = req.headers.get("accept");
+	let message = ip;
+	let contentType = "text/html";
+
+	if (accept?.includes("application/json")) {
+		message = JSON.stringify(ip);
+		contentType = "application/json";
+	}
+
+	return new Response(message.concat("\n"), {
+		status: 200,
+		headers: { ...headers, "Content-Type": contentType },
+	});
+}
+
+// Common headers:
+const headers = {
+	Vary: "*", // See https://stackoverflow.com/a/54337073 for why "Vary: *" is necessary for Safari
+	"Cache-Control": "no-store",
+	"X-Clacks-Overhead": "GNU Terry Pratchett",
+};
+
+const cors = {
+	"Access-Control-Allow-Methods": "GET, OPTIONS",
+	"Access-Control-Allow-Headers": "Accept, Content-Length, Content-Type, Date",
+	"Access-Control-Allow-Origin": "*",
+};
+
+// Vercel wants the endpoint to be a `default` export:
+export default function handler(req: Request): Response {
+	switch (req.method.toUpperCase()) {
+		// Normal requests:
+		case "GET":
+			return echo(req);
 
 		// CORS preflight:
 		case "OPTIONS":
-			return new Response(undefined, { status: 200, headers });
+			return new Response(undefined, { status: 204, headers: { ...headers, ...cors } });
 
 		// Everything else:
 		default:
@@ -35,18 +53,7 @@ export function echo(req: Request): Response {
 	}
 }
 
-const headers = {
-	"Cache-Control": "no-store",
-	Vary: "*", // See https://stackoverflow.com/a/54337073 for why "Vary: *" is necessary for Safari
-	"X-Clacks-Overhead": "GNU Terry Pratchett",
-	"Access-Control-Allow-Headers":
-		"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-	"Access-Control-Allow-Origin": "*",
-};
-
 // Vercel Edge config:
-export default echo;
-
 export const config = {
 	runtime: "edge",
 };
